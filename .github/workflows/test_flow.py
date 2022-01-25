ad3ad7eb83220b85aa8f23a793a09fa245e715cc
Zachary Schutzman <zschutzman@users.noreply.github.com>
2022-01-25 10:47:14 -0500

    Update test_flow.py

--- .github/workflows/test_flow.py
+++ .github/workflows/test_flow.py
@@ -2,6 +2,6 @@ import pandas as pd
 import os
 
 
-df = pd.read_csv("../../_data/aggregate.csv")
+df = pd.read_csv("_data/aggregate.csv")
 df = df.rename(columns=lambda x: x.strip())
 print(list(df))
