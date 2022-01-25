8b574021b478e03dadcafaf153eb7408e4cf0854
Zachary Schutzman <zschutzman@users.noreply.github.com>
2022-01-25 10:49:46 -0500

    Update test_flow.py

--- .github/workflows/test_flow.py
+++ .github/workflows/test_flow.py
@@ -5,3 +5,25 @@ import os
 df = pd.read_csv("_data/hot-or-not/aggregate.csv")
 df = df.rename(columns=lambda x: x.strip())
 print(list(df))
+
+
+for f in os.listdir("_data/hot-or-not/raw"):
+    if f[:4] == "dat_":
+        for l in open("_data/hot-or-not/raw/"+f,'r').readlines():
+            ll = l.strip().replace("'",'').split(', ')
+            #print(ll[0])
+            df.loc[df.district == ll[0],ll[1]] += 1
+
+
+df.to_csv("_data/hot-or-not/aggregate2.csv",index=False)
+
+js_str = "var vote_data = {"
+
+for i,r in df.iterrows():
+
+    js_str += ("'" + str(r["district"]) + "'" + ": [" + str(r["hot"]) + ", " + str(r["not"]) + "], ")
+
+js_str = js_str[:-2] + '}'
+
+with open("_data/hot-or-not/testjs.txt", 'w') as jsout:
+    jsout.write(js_str)
